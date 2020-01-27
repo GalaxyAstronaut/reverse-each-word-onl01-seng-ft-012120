4007f7e4eaf6b76c253e0fa9edb09bf9b8305b9c
Oscar Zapata <0northbridge.cpu1@gmail.com>
2020-01-27 15:19:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,7 @@
 def reverse_each_word(string)
   word_array = string.split(" ")
   new_arr = [] 
-  word_array.each do |word|
-    new_arr.push(word.reverse)
-  end
+  new_arr = word_array.collect
   new_arr.join(" ")
   
 end
